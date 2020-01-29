eda4ea016fe55230e166704c9b870fa1ba93054a
tiktok <mehdi-098@outlook.fr>
2020-01-29 22:18:59 +0100

    add pushfront

--- vector.c
+++ vector.c
@@ -2,23 +2,25 @@
 
 	
 vector initvector(size_t capacity)
-{	/*if(capacity<10)
-		capacity=10;*/
+{	if(capacity<1)
+		capacity=2;
+	
 	vector new;
-	new.data=(int*)malloc(sizeof(int)*capacity);
-	new.capacity=capacity;
-	new.size=0;
+	new.data = (int*) malloc(sizeof(int) * capacity);
+	assert(new.data);
+	new.capacity = capacity;
+	new.size = 0;
 	return new;
-
 }
 
 void copy(vector *dest , vector *src)
 {
-	if(dest->capacity<src->size)
+	if(dest->capacity<=src->size)
 	{
 		fprintf(stderr, "%s\n", "capacity<size");
-		return;
+		exit(1);
 	}
+	
 	size_t i;
 	for ( i = 0; i < src->size; ++i)
 		dest->data[i]=src->data[i];
@@ -29,46 +31,79 @@ void fill(vector* t , int v){
 	size_t i;
 	for ( i = 0; i < t->capacity; ++i)
 		t->data[i]=v;
-
+	
 	t->size=t->capacity-1;
 }
 
-void printvector(vector t)
+void printvector(vector *t)
 {
 	size_t i;
-	for (i = 0; i < t.size; ++i)
-		printf("%d\n",t.data[i]);
+	for (i = 0; i < t->size; ++i)
+		printf("%ld : %d\n",i,t->data[i]);
 }
 
 void reserve(vector *t)
 {
-	vector*tmp;
-	tmp->data=(int*)malloc(sizeof(int)*t->capacity);
-	tmp->capacity=t->capacity;
-	tmp->size=t->capacity;
-	copy(tmp,t);
+	vector  tmp;
+	tmp.data = (int*) malloc( sizeof(int) * t->capacity );
+	if(tmp.data==NULL)
+	{
+		fprintf(stderr, "erreur d'allocation\n" );
+		exit(0);
+	}
+	assert(tmp.data);
+	tmp.capacity = t->capacity;
+	tmp.size = t->capacity;
+	copy(&tmp,t);
 	free(t->data);
-	t->data=(int*)malloc(sizeof (int)*t->capacity*2);
-	t->capacity*=2;
-	printf("entre dans reserve %d\n",t->capacity);
-	copy(t,tmp);
+	t->data = (int*)malloc(sizeof (int)*t->capacity*2);
+	assert(t->data);
+	t->capacity *= 2;
+	copy(t,&tmp);
 }
 
 void pushback(vector *t,int v)
-{	if(t->size>=t->capacity)
+{	
+	if(t->size >= t->capacity-1)
 		reserve(t);
-	t->data[t->size]=v;
+	
+	t->data[t->size] = v ;
 	t->size++;	
+}
+
+void pushfront(vector*t,int v)
+{	
+	if( t->size >= t->capacity - 1 )
+		reserve(t);
+	size_t i;
+	for ( i = t->size ; i >0 ; --i )
+		t->data[i] = t->data[i-1];
+	t->data[0] = v;
+	t->size++;
+	
+}
+
+int front(vector*t){
+	return t->data[0];
+} 
+
+int back(vector*t)
+{
+	return t->data[t->size-1];
+}
+
+size_t size(vector*t)
+{
+	return t->size;
+}
+
+size_t capacity(vector*t)
+{
+	return t->capacity;
 }
 
 void freevector(vector *t)
 {
 	free(t->data);
-
 }
 
-int main(int argc, char const *argv[])
-{
-
-	return 0;
-} 
--- vector.h
+++ vector.h
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include <errno.h>
 typedef struct vector
-{
+{	
 	int *data;
 	size_t size,capacity;
 }vector;
 vector initvector(size_t capacity);
 void copy(vector *dest , vector *src);
 void fill(vector* t , int v);
-void printvector(vector t);
+void printvector(vector* t);
 void reserve(vector *t);
+void pushfront(vector*t,int v);
 void pushback(vector *t,int v);
+int front(vector*t);
+int back(vector*t);
 void freevector(vector *t);
+size_t size(vector*);
+size_t capacity(vector*);
